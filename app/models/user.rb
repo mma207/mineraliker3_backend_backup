e4fce08b7f5ee6abe8f055cf3d5bb2a21711dcbe
Muna Mashhour <munamashhour@gmail.com>
2020-01-22 14:39:42 -0500

    message colors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy 
+    has_many :messages, dependent: :destroy 
 
     has_secure_password
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_154425) do
+ActiveRecord::Schema.define(version: 2020_01_22_184401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,9 @@ ActiveRecord::Schema.define(version: 2020_01_21_154425) do
     t.bigint "conversation_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_01_21_154425) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
